0b5408018259f3f023e83d1a85252dda97c0c90e
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-24 16:10:57 +0100

    try-fix: pchar

--- pchar.c
+++ pchar.c
@@ -8,23 +8,20 @@
 */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	int value = (*stack)->n;
+	int value;
 
 	if (*stack == NULL || stack == NULL)
 	{
-		fclose(data.bytecode_file);
-		free(data.line);
-		clean_s(*stack);
-		error(EXIT_FAILURE, "L%u: can't pchar, stack empty\n",
-				(void *)&line_number, 'u');
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
+
+	value = (*stack)->n;
+
 	if (value < 0 || value >= 128)
 	{
-		fclose(data.bytecode_file);
-		free(data.line);
-		clean_s(*stack);
-		error(EXIT_FAILURE, "L%u: can't pchar, value out of range\n",
-				(void *)&line_number, 'u');
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 
 	printf("%c\n", value);
