5ae65661e747b209f366ff53eae6dd725db1ae42
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-24 14:24:11 +0100

    Fix: pstr

--- pstr.c
+++ pstr.c
@@ -9,17 +9,13 @@
 void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
-	char str[1024];
-	int i = 0;
 
 	(void)line_number;
 	while (current != NULL && current->n != 0 && current->n > 0
 		&& current->n <= 127)
 	{
-		str[i] = current->n;
-		i++;
+		printf("%c", current->n);
 		current = current->next;
 	}
-	str[i] = '\0';
-	printf("%s\n", str);
+	printf("\n");
 }
