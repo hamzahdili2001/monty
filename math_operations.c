1c40a9f2bc0c4f397bfd531edaa55fcbdc1b1d33
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-24 13:26:14 +0100

    clean: the code

--- math_operations.c
+++ math_operations.c
@@ -11,9 +11,6 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fclose(data.bytecode_file);
-		free(data.line);
-		clean_s(*stack);
 		error(EXIT_FAILURE, "L%u: can't add, stack too short\n",
 				(void *)&line_number, 'u');
 	}
@@ -33,9 +30,6 @@ void sub(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fclose(data.bytecode_file);
-		free(data.line);
-		clean_s(*stack);
 		error(EXIT_FAILURE, "L%u: can't sub, stack too short\n",
 				(void *)&line_number, 'u');
 	}
@@ -54,9 +48,6 @@ void divide(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fclose(data.bytecode_file);
-		free(data.line);
-		clean_s(*stack);
 		error(EXIT_FAILURE, "L%u: can't div, stack too short\n",
 				(void *)&line_number, 'u');
 	}
@@ -99,9 +90,6 @@ void mod(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fclose(data.bytecode_file);
-		free(data.line);
-		clean_s(*stack);
 		error(EXIT_FAILURE, "L%u: can't mod, stack too short\n",
 				(void *)&line_number, 'u');
 	}
