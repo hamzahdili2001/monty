640bcd31081a05006926d00d169fd8354630f482
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-24 13:55:25 +0100

    add: pstr

--- monty.h
+++ monty.h
@@ -63,6 +63,7 @@ void mul(stack_t **stack, unsigned int line_number);
 void divide(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 bool is_number(const char *str, int *value);
 int error(int code, char *msg, void *arg, char type);
---
+++ pstr.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * pstr - prints the string starting at the top of the stack,
+ *				followed by a new line.
+ * @stack: stack.
+ * @line_number: line number.
+ * Return: Nothing.
+*/
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	int value;
+
+	(void)line_number;
+	while (current != NULL && current->n != 0 && current->n >= 0
+		&& current->n <= 127)
+	{
+		value = current->n;
+		printf("%c", value);
+		current = current->next;
+	}
+	printf("\n");
+}
